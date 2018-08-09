f62523b568269214eeb71b1c59adb6d1808b09a7
voak <words2line@gmail.com>
2018-08-09 19:19:35 +0100

    EOD-09082018

--- Classes.js
+++ Classes.js
@@ -34,7 +34,86 @@ var Max = /** @class */ (function (_super) {
     function Max(name) {
         var _this = _super.call(this, name, age, name) || this;
         _this.name = 'Maxi';
+        _this.age = 37;
         return _this;
     }
     return Max;
 }(Person));
+//Getters and Setters
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this._species = "Default";
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        get: function () {
+            return this._species;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+            else {
+                this._species = "Defualt";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var plant = new Plant();
+console.log(plant.species);
+plant.species = 'Ab';
+console.log(plant.species);
+plant.species = 'Abcd';
+console.log(plant.species);
+//Static Properties & Methods
+var Helpers = /** @class */ (function () {
+    function Helpers() {
+    }
+    Helpers.calCircum = function (diameter) {
+        return this.PI * diameter;
+    };
+    Helpers.PI = 3.14;
+    return Helpers;
+}());
+console.log(Helpers.PI);
+console.log(Helpers.calCircum(87.2));
+var Project = /** @class */ (function () {
+    function Project() {
+        this.projectName = "Default";
+        this.budget = 1000;
+    }
+    Project.prototype.calcBudget = function () {
+        return this.budget * 2;
+    };
+    return Project;
+}());
+var ITProject = /** @class */ (function (_super) {
+    __extends(ITProject, _super);
+    function ITProject() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ITProject.prototype.changeName = function (name) {
+        this.projectName = name;
+    };
+    return ITProject;
+}(Project));
+var newProject = new ITProject();
+console.log(newProject);
+newProject.changeName("Super IT Project");
+console.log(newProject);
+//In cases where only a single instance is required, then
+var OnlyOne = /** @class */ (function () {
+    function OnlyOne(name) {
+    }
+    OnlyOne.getInstance = function () {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne("The Only one");
+        }
+        return OnlyOne.instance;
+    };
+    return OnlyOne;
+}());
+//let noinstance = new OnlyOne("The only one"); //Wrong
+var right = OnlyOne.getInstance();
--- Classes.ts
+++ Classes.ts
@@ -30,5 +30,89 @@ class Max extends Person {
 
     constructor(name: string){
         super(name, age, name);
+        this.age = 37; 
     }
 }
+
+
+//Getters and Setters
+
+class Plant {
+
+    private _species: string = "Default";
+    set species(value:string) {
+        if(value.length > 3){
+            this._species = value;
+        }
+        else{
+            this._species = "Defualt";
+        }
+    }
+
+    get species() {
+        return this._species;
+    }
+}
+
+
+let plant = new Plant();
+console.log(plant.species);
+
+plant.species = 'Ab';
+console.log(plant.species);
+
+plant.species = 'Abcd';
+console.log(plant.species);
+
+//Static Properties & Methods
+class Helpers {
+    static PI: number = 3.14;
+
+    static calCircum(diameter: number) : number{
+        return this.PI * diameter;
+    }
+}
+
+console.log(Helpers.PI);
+console.log(Helpers.calCircum(87.2));
+
+abstract class Project {
+    projectName: string = "Default";
+    budget: number = 1000;
+
+    abstract changeName(name: string): void;
+
+    calcBudget() {
+        return this.budget * 2;
+    }
+}
+
+class ITProject extends Project {
+    changeName(name: string): void {
+        this.projectName = name;
+    }
+}
+
+let newProject = new ITProject();
+console.log(newProject);
+newProject.changeName("Super IT Project");
+console.log(newProject);
+
+//In cases where only a single instance is required, then
+
+class OnlyOne {
+
+    public static instance: OnlyOne;
+
+    private constructor(name: string){}
+
+    static getInstance() {
+        if(!OnlyOne.instance){
+            OnlyOne.instance = new OnlyOne("The Only one");
+        }
+        return OnlyOne.instance;
+    }
+}
+
+//let noinstance = new OnlyOne("The only one"); //Wrong
+let right = OnlyOne.getInstance();
